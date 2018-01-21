96b0c5097e2a2b95159d0f855358495101eca5c9
John Culpin <john@johnculpin.co.uk>
2018-01-21 17:16:24 +0000

    Initial commit to check GitHub setup

---
+++ first.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main ()
+{
+    cout << sqrt(10) << "\n";
+    cout << pow ( 2, 4) << "\n";
+    cout << exp (12) << "\n";
+    cout << M_PI << "\n";
+}
