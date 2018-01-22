481a53ff9b8346a1a4791cd3aa3d6897808b769c
John Culpin <john@johnculpin.co.uk>
2018-01-22 12:33:33 +0000

    Tidy up directory (seperate dir for executables)

--- first.cpp
+++ first.cpp
@@ -5,8 +5,17 @@ using namespace std;
 
 int main ()
 {
+    double x = 7.8, y = 1.65, u = -1.4, z;
+    
     cout << sqrt(10) << "\n";
     cout << pow ( 2, 4) << "\n";
     cout << exp (12) << "\n";
     cout << M_PI << "\n";
+    
+    z = fmod(x, y);
+    cout << z << "\n";
+    z = atan2(y, x);
+    cout << z << "\n";
+    z = fabs(u);
+    cout << z << "\n";
 }
